d8cda147c7b464144c411adbf4210c4c7bfc315b
wmau <wmau@bu.edu>
2016-02-22 13:32:57 -0500

    Skeleton code
    Null results here, but saving anyway. This function looks for new time
    cells and asks where the time cells are that have tuning curve close to
    that of the new time cell.

--- RemapProx.m
+++ RemapProx.m
@@ -48,18 +48,18 @@ function RemapProx(tday,yday,MAPlocation,Ts)
     
     %Plot neurons sized by when their time field occurs. Larger = later in
     %the delay. 
-    figure;
-    scatter(tday.centroids(tday.new.inds,1),...
-        tday.centroids(tday.new.inds,2),...
-        tday.new.tpks_scaled); hold on;
-    scatter(tday.centroids(tday.newtuning.inds,1),...
-        tday.centroids(tday.newtuning.inds,2),...
-        tday.newtuning.tpks_scaled); hold on;
-    scatter(yday.centroids(TIMECELLS{1},1),...
-        yday.centroids(TIMECELLS{1},2),...
-        yday.tpks_scaled); 
-    xlim([0 Xdim]); ylim([0 Ydim]); axis off; 
-    title('New Time Fields'); legend({'New Time Cells','New Tuning'}); 
+%     figure;
+%     scatter(tday.centroids(tday.new.inds,1),...
+%         tday.centroids(tday.new.inds,2),...
+%         tday.new.tpks_scaled); hold on;
+%     scatter(tday.centroids(tday.newtuning.inds,1),...
+%         tday.centroids(tday.newtuning.inds,2),...
+%         tday.newtuning.tpks_scaled); hold on;
+%     scatter(yday.centroids(TIMECELLS{1},1),...
+%         yday.centroids(TIMECELLS{1},2),...
+%         yday.tpks_scaled); 
+%     xlim([0 Xdim]); ylim([0 Ydim]); axis off; 
+%     title('New Time Fields'); legend({'New Time Cells','New Tuning'}); 
 
     %Plot where the new time cells are. Red = new time cells. Blue = time
     %cells that changed their tuning. 
@@ -70,14 +70,17 @@ function RemapProx(tday,yday,MAPlocation,Ts)
 %     title('New Time Tuning Topography'); 
     
 %% 
-    window = 0.1; 
+    window = 0.26; 
+    dall = []; 
+    
     keepgoing = 1;
     i = 1;
-    f = figure;
+    f = figure('Position',[520 70 550 730]);
     while keepgoing
         yInfield = TIMECELLS{1}(yday.tpks < tday.new.tpks(i) + window & ...
             yday.tpks > tday.new.tpks(i) - window);
         
+        subplot(3,2,1:4);
         scatter(tday.centroids(tday.new.inds(i),1),...
             tday.centroids(tday.new.inds(1),2),...
             tday.new.tpks_scaled(i)); hold on;
@@ -87,16 +90,58 @@ function RemapProx(tday,yday,MAPlocation,Ts)
         xlim([0 Xdim]); ylim([0 Ydim]); axis off; 
         hold off; 
         
+        subplot(3,2,5);
+        d = pdist([tday.centroids(tday.new.inds(i),:); yday.centroids(yInfield,:)]); 
+        d = d(1:length(yInfield));       
+        dall = [dall d]; 
+        
+        if ~isempty(d)
+            null = pdist([tday.centroids(tday.new.inds(i),:); yday.centroids(TIMECELLS{1},:)]); 
+            null = null(1:length(TIMECELLS{1}));
+            h1 = histogram(d);                  hold on;
+            h2 = histogram(null);               hold off; 
+            h1.Normalization = 'probability';   h1.BinWidth = 20; 
+            h2.Normalization = 'probability';   h2.BinWidth = 20; 
+            ylim([0 1]); 
+
+            subplot(3,2,6); 
+            [H,pval] = kstest2(d,null); 
+            ecdf(d);                            hold on;
+            ecdf(null);                         hold off; 
+            title(['p=',num2str(pval)]); 
+        else 
+            clf; 
+        end
+        
         [keepgoing,i] = scroll(i,length(tday.new.inds),f); 
         
     end
-            
+    
+    figure('position',[520    49   560   768]); 
+    
+    null = pdist(tday.centroids(tday.new.inds,:)); 
+    
+    subplot(2,1,1); 
+    h1 = histogram(dall);               hold on;
+    h2 = histogram(null);               hold off; 
+    h1.Normalization = 'probability';   h1.BinWidth = 20; 
+    h2.Normalization = 'probability';   h2.BinWidth = 20; 
+    
+    subplot(2,1,2); 
+    [~,pval] = kstest2(dall,null); 
+    ecdf(dall); hold on;
+    ecdf(null); 
+    title(['p=',num2str(pval)]); 
+
+    keepgoing = 1;
     i = 1;
-    keepgoing = 1; 
+    f = figure('Position',[520 70 550 730]);
+    dall = []; 
     while keepgoing
         yInfield = TIMECELLS{1}(yday.tpks < tday.newtuning.tpks(i) + window & ...
             yday.tpks > tday.newtuning.tpks(i) - window);
         
+        subplot(3,2,1:4);
         scatter(tday.centroids(tday.newtuning.inds(i),1),...
             tday.centroids(tday.newtuning.inds(1),2),...
             tday.newtuning.tpks_scaled(i)); hold on;
@@ -106,9 +151,48 @@ function RemapProx(tday,yday,MAPlocation,Ts)
         xlim([0 Xdim]); ylim([0 Ydim]); axis off; 
         hold off; 
         
+        subplot(3,2,5);
+        d = pdist([tday.centroids(tday.newtuning.inds(i),:); yday.centroids(yInfield,:)]); 
+        d = d(1:length(yInfield));
+        dall = [dall d];
+        
+        if ~isempty(d)
+            null = pdist([tday.centroids(tday.newtuning.inds(i),:); yday.centroids(TIMECELLS{1},:)]); 
+            null = null(1:length(TIMECELLS{1}));
+            h1 = histogram(d);                  hold on;
+            h2 = histogram(null);               hold off; 
+            h1.Normalization = 'probability';   h1.BinWidth = 20; 
+            h2.Normalization = 'probability';   h2.BinWidth = 20; 
+            ylim([0 1]); 
+
+            subplot(3,2,6); 
+            [H,pval] = kstest2(d,null); 
+            ecdf(d);                            hold on;
+            ecdf(null);                         hold off; 
+            title(['p=',num2str(pval)]); 
+        else 
+            clf; 
+        end
+        
         [keepgoing,i] = scroll(i,length(tday.newtuning.inds),f); 
         
-    end   
-    keyboard; 
+    end
+   
+    figure('position',[520    49   560   768]); 
     
+    null = pdist([tday.centroids(tday.new.inds(i),:); yday.centroids(yInfield,:)]); 
+    
+    subplot(2,1,1); 
+    h1 = histogram(dall);               hold on;
+    h2 = histogram(null);               hold off; 
+    h1.Normalization = 'probability';   h1.BinWidth = 20; 
+    h2.Normalization = 'probability';   h2.BinWidth = 20; 
+    
+    subplot(2,1,2); 
+    [~,pval] = kstest2(dall,null); 
+    ecdf(dall); hold on;
+    ecdf(null); 
+    title(['p=',num2str(pval)]); 
+
+   
 end
