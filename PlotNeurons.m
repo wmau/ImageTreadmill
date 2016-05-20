407747942872162cbbd0c313a1678000e35410fc
wmau <wmau@bu.edu>
2016-05-20 16:55:20 -0400

    Tweak PlotNeurons

--- PlotNeurons.m
+++ PlotNeurons.m
@@ -1,4 +1,4 @@
-function PlotNeurons(NeuronImage,Xdim,Ydim,c,thickness)
+function PlotNeurons(md,neurons,col,thickness)
 %PlotNeurons(NeuronImage)
 %
 %   Given cell arrays of binary images, plot the outlines of neurons. 
@@ -11,16 +11,32 @@ function PlotNeurons(NeuronImage,Xdim,Ydim,c,thickness)
 %
 
 %% Plot neurons. 
-    nNeurons = length(NeuronImage);
+    cd(md.Location); 
+    load('ProcOut.mat','NeuronImage','NumNeurons','Xdim','Ydim');
+    try
+        load('ProcOut.mat','xOutline','yOutline'); 
+        xOutline{1};
+    catch
+        try 
+            load('PlaceMaps.mat','xOutline','yOutline');
+            xOutline{1};
+        catch
+            xOutline = cell(NumNeurons,1);
+            yOutline = cell(NumNeurons,1); 
+
+            for i=1:NumNeurons
+                b = bwboundaries(NeuronImage{i});
+                xOutline{i} = b{1}(:,1); 
+                yOutline{i} = b{1}(:,2);
+            end
+            
+            save('ProcOut.mat','xOutline','yOutline','-append');
+        end
+    end
     
     hold on; 
-    for i=1:nNeurons
-        b = bwboundaries(NeuronImage{i});
-        
-        x = b{1}(:,1);
-        y = b{1}(:,2);
-        
-        plot(y,x,'b','color',c,'linewidth',thickness); 
+    for i=neurons
+        plot(yOutline{i},xOutline{i},'color',col,'linewidth',thickness); 
     end
     hold off; 
     xlim([0 Xdim]); ylim([0 Ydim]); axis off; 
