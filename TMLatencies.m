ecf9c22a0a9d143bcc8a97cf8831a34f68a4efad
wmau <wmau@bu.edu>
2016-07-26 11:37:11 -0400

    Functionalized treadmill-target latencies

---
+++ TMLatencies.m
@@ -0,0 +1,30 @@
+function TMAlignedOnsets = TMLatencies(immRaster,targRaster)
+%TMAlignedOnsets = TMLatencies(immRaster,targRaster)
+%
+%   Gets the treadmill-target latencies for laps where both the trigger and
+%   target cells are active. 
+%
+%   INPUTS
+%       immRaster: trigger raster with all spikes removed except for those
+%       immediately preceding spikes from the target. 
+%
+%       targRaster: target cell raster.
+%
+%   OUTPUT
+%       TMAlignedOnsets: Treadmill-target latencies (in seconds). 
+%
+
+%% Main body. 
+    bothActiveLaps = find(any(immRaster,2)); 
+    TMAlignedOnsets = [];
+    
+    %Only on laps where both are active. 
+    for l=bothActiveLaps'
+        %Get the onset times of each neuron. 
+        TMAlignedOnsets = [TMAlignedOnsets find(targRaster(l,:))];    
+    end
+    
+    %Divide by frame rate. 
+    TMAlignedOnsets = TMAlignedOnsets ./ 20; 
+    
+end
