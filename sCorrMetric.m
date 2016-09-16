2ddbb9743889d0bcbb7d4341be15ad96032ca94b
wmau <wmau@bu.edu>
2016-09-16 14:19:59 -0400

    Correlation metric

---
+++ sCorrMetric.m
@@ -0,0 +1,31 @@
+function stability = sCorrMetric(normtilemat)
+%stability = sCorrMetric(normtilemat)
+%
+%   Computes the Spearman correlation of each time cell in the first cell
+%   array with itself on other cell arrays (other sessions). 
+%
+%   INPUT
+%       normtilemat: output from multiPastalkovaPlot.
+%
+%   OUTPUT
+%       stability: TCxS matrix of correlation coefficients for each time
+%       cell across sessions. First column is always 1.  
+%
+
+%% Do correlations. 
+    nSessions = length(normtilemat);
+    nTCs = size(normtilemat{1},1);
+    
+    %Preallocate and assign first column all 1s. 
+    stability = nan(nTCs,nSessions);
+    stability(:,1) = 1; 
+    for s=2:nSessions
+        for n=1:nTCs
+            %Spearman (non-parametric) correlation.
+            stability(n,s) = corr(normtilemat{1}(n,:)',normtilemat{s}(n,:)','type','Spearman');
+        end
+    end
+
+    %Set NaNs to 0. 
+    stability(isnan(stability)) = 0;
+end
