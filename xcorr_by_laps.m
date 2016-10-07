ff8550d761b6b369d7e3c3d726ed82a02705df56
wmau <wmau@bu.edu>
2016-10-07 14:02:28 -0400

    Now takes max lag of +/- 4s

--- xcorr_by_laps.m
+++ xcorr_by_laps.m
@@ -11,15 +11,18 @@ function [r,lags]=xcorr_by_laps(triggerRaster,targetRaster)
 %       r: LxT (L=# of laps, T=# of lag bins) matrix for cross
 %       correlations at each lap.
 %
-%       lags: Vector of lags. 
+%       lags: Vector of lags, in frames. 
 %
 
 %% Perform cross correlation.
     nLaps = size(triggerRaster,1); 
+    maxlag_seconds = 4;             %Default hard code.
+    maxlag = maxlag_seconds*20;     %Translate into frames. 
     
-    r = zeros(nLaps,399); 
+    %XCorr. 
+    r = zeros(nLaps,maxlag*2+1); 
     for l=1:nLaps
-        [r(l,:),lags] = xcorr(triggerRaster(l,:),targetRaster(l,:),'coeff'); 
+        [r(l,:),lags] = xcorr(triggerRaster(l,:),targetRaster(l,:),maxlag,'coeff'); 
     end
     
     %Lags is in frames. 
