17faac070b0c3e2c04a39602a3db8a43796420df
wmau <wmau@bu.edu>
2017-02-02 11:22:16 -0500

    Streamlined.

--- PartitionStats.m
+++ PartitionStats.m
@@ -25,6 +25,7 @@ function [STATS,nNeurons,stable,unstable] = PartitionStats(mds,stabilityType,sta
     animals = unique({mds.Animal});
     nAnimals = length(animals); 
     statType = lower(statType);
+    
 
 %% Compile
     [STATS.stable,STATS.unstable,stable,unstable] = deal(cell(1,nAnimals));
@@ -32,106 +33,105 @@ function [STATS,nNeurons,stable,unstable] = PartitionStats(mds,stabilityType,sta
     for a = 1:nAnimals       
         %Get all the sessions for this animal.
         ssns = find(strcmp(animals{a},{mds.Animal})); 
-        
+    
         [nStable,nUnstable] = deal(0); 
         [STATS.stable{a},STATS.unstable{a}] = deal([]);
         [stable{a},unstable{a}] = deal(cell(1,length(ssns)-1));
         for s = 1:length(ssns)-1
             cd(mds(ssns(s)).Location);
             
+            %p-value criterion to be considered a place cell. 
             PCcrit = .01;
             
             %Get place and time cells.
             PCs = getPlaceCells(mds(ssns(s)),PCcrit);
             TCs = getTimeCells(mds(ssns(s)));
                         
-            if strcmp(statType,'ti')
-                load('TemporalInfo.mat','MI','Ispk','Isec');
-                stat = MI;                
-                
-                if strcmp(stabilityType,'time')
-                    neurons = TCs;
-                elseif strcmp(stabilityType,'place')
-                    neurons = intersect(TCs,PCs);
-                end
-            elseif strcmp(statType,'si')
-                load('SpatialInfo.mat','MI','Ispk','Isec');
-                stat = MI;
-                
-                if strcmp(stabilityType,'time')
-                    neurons = intersect(TCs,PCs);
-                elseif strcmp(stabilityType,'place')
-                    neurons = PCs;
-                end
-                
-            elseif strcmp(statType,'pk')
-                [~,stat] = getTimePeak(mds(ssns(s)));
-                
-                if strcmp(stabilityType,'time')
-                    neurons = TCs;
-                end           
-                
-            elseif strcmp(statType,'fr')
-                load('Pos_align.mat','PSAbool');
-                [n,f] = size(PSAbool);
-%                 d = diff([zeros(n,1) PSAbool],1,2);
-%                 d(d<0) = 0;
-                stat = sum(PSAbool,2)./f; 
-                
-                if strcmp(stabilityType,'time')
-                    neurons = TCs;
-                elseif strcmp(stabilityType,'place')
-                    neurons = PCs;
-                end
+            switch statType
+                %Temporal information. 
+                case 'ti' 
+                    load('TemporalInfo.mat','MI','Ispk','Isec');
+                    stat = MI;                
 
-%                 load('Pos_align.mat','DFDTtrace');
-%                 stat = mean(DFDTtrace,2);
+                    %If looking at temporal information and time stability,
+                    %get all time cells. Otherwise, get dual time/place
+                    %cells. 
+                    switch stabilityType 
+                        case 'time',neurons = TCs;
+                        case 'place',neurons = intersect(TCs,PCs);
+                    end
+                    
+                %Spatial information.
+                case 'si'
+                    load('SpatialInfo.mat','MI','Ispk','Isec');
+                    stat = MI;
 
-%                 load(fullfile(pwd,'TimeCells.mat'),'TodayTreadmillLog','T');
-%                 load(fullfile(pwd,'Pos_align.mat'),'PSAbool');
-%                 inds = TrimTrdmllInds(TodayTreadmillLog,T);
-%                 n = size(PSAbool,1);
-%                 rasters = cell(1,n);
-%                 stat = zeros(n,1);
-%                 for nn=1:n
-%                     rasters{nn} = buildRaster(inds,PSAbool,nn,'onsets',false);
-%                     stat(nn) = sum(rasters{nn}(:))./numel(rasters{nn});
-%                 end                       
+                    %If looking at spatial information and spatial
+                    %stability, get all place cells. Otherwise, get dual
+                    %time/place cells. 
+                    switch stabilityType 
+                        case 'time',neurons = intersect(TCs,PCs);
+                        case 'place',neurons = PCs;
+                    end
+                
+                %Firing rate (really Ca event rate).
+                case 'fr'
+                    load('Pos_align.mat','PSAbool');
+                    [n,f] = size(PSAbool);
+    %                 d = diff([zeros(n,1) PSAbool],1,2);
+    %                 d(d<0) = 0;
+                    stat = sum(PSAbool,2)./f; 
+                    
+                    %Select cells based on stability type. 
+                    switch stabilityType
+                        case 'time', neurons = TCs;
+                        case 'place',neurons = PCs;
+                    end
+                    
+                %Mean fluorescence.
+                case 'fluor'
+                    load('Pos_align.mat','DFDTtrace');
+                    stat = mean(DFDTtrace,2);
+                
+                    %Select cells based on stability type. 
+                    switch stabilityType
+                        case 'time', neurons = TCs;
+                        case 'place',neurons = PCs;
+                    end
+                    
+                %Time peak. 
+                case 'pk'
+                    [~,stat] = getTimePeak(mds(ssns(s)));
+
+                    %Should only ever be time cells. 
+                    neurons = TCs;                              
             end
             
             %stat = (stat-min(stat))./range(stat);
-            if strcmp(statType,'fr')
+            %Normalize.
+            if any(strcmp(statType,{'fr','fluor'}))
                 stat = (stat-min(stat))./range(stat);
             else
                 stat(neurons) = zscore(stat(neurons));
             end
-            
-                       
-            if strcmp(stabilityType,'time')           
-                %Get correlation coefficients and p-values. 
-                corrs = CorrTrdmllTrace(mds(ssns(s)),mds(ssns(s+1)),neurons);
-                
-                %[~,stblcrit] = fdr_bh(corrStats(~isnan(corrStats(:,2)),2));
-                stblcrit = .01/length(neurons);
+                           
+            switch stabilityType
+                case 'time'           
+                    %Get correlation coefficients and p-values. 
+                    corrs = CorrTrdmllTrace(mds(ssns(s)),mds(ssns(s+1)),neurons);
 
-                %Stable time cells based on correlation and non-shifting time
-                %field.
-                stable{a}{s} = intersect(find(corrs(:,2) < stblcrit),neurons);
-                unstable{a}{s} = intersect(find(corrs(:,2) >= stblcrit | isnan(corrs(:,2))),neurons);
-         
-            elseif strcmp(stabilityType,'place')
-                
-                %Get the correlation coefficients and p-values.
-                corrs = CorrPlaceFields(mds(ssns(s)),mds(ssns(s+1)),neurons);
-                
-                %[~,stblcrit] = fdr_bh(corrStats(~isnan(corrStats(:,2)),2));
-                stblcrit = .01/length(neurons);
-                
-                %Stable place cells based on correlation p-value.
-                stable{a}{s} = intersect(find(corrs(:,2) < stblcrit),neurons); 
-                unstable{a}{s} = intersect(find(corrs(:,2) >= stblcrit | isnan(corrs(:,2))),neurons);     
+                case 'place'
+                    %Get the correlation coefficients and p-values.
+                    corrs = CorrPlaceFields(mds(ssns(s)),mds(ssns(s+1)),neurons);       
             end
-           
+            
+            %[~,stblcrit] = fdr_bh(corrStats(~isnan(corrStats(:,2)),2));
+            stblcrit = .01/length(neurons);
+
+            %Stable time cells based on correlation.
+            stable{a}{s} = intersect(find(corrs(:,2) < stblcrit),neurons);
+            unstable{a}{s} = intersect(find(corrs(:,2) >= stblcrit | isnan(corrs(:,2))),neurons);
+                 
             %Get number of stable and unstable cells. Add this one per
             %session but track the number per animal. 
             nStable = nStable + length(stable{a}{s}); 
