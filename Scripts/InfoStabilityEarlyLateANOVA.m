24211323beda28f95f529f1533a435e1af4c8ead
wmau <wmau@bu.edu>
2017-02-15 19:02:21 -0500

    Early late distinction and new stability over days functions

---
+++ Scripts/InfoStabilityEarlyLateANOVA.m
@@ -0,0 +1,148 @@
+%% Set up.
+    clear;
+    
+    %Load MD entries. 
+    loadMD;
+    %MD(292:295) = G45.
+    %MD(296:299) = G48.
+    %MD(300:304) = Bellatrix.
+    %MD(305:309) = Polaris.
+    fulldataset = MD(292:309);      
+
+    %Some initial variables. 
+    teal = [0 .5 .5];
+    purple = [.58 .44 .86];
+    
+%% Save information
+    saveBool = false;
+    folder = 'C:\Users\William Mau\Documents\Projects\Time Cell Imaging Summer 2015 -\Paper\Figures\Supplementals\Alternative Info Stability Analyses';
+    Time = fullfile(folder,'Stable Time TI Medians');
+    TimeFR = fullfile(folder,'Stable Time FR Medians');
+    
+    if saveBool
+        c = input('Saving set to true. Are you sure you want to continue? (y/n)','s');
+    
+        if ~strcmp(c,'y')
+            saveBool = false;
+        end
+    end
+    
+ %% Main analysis - early.
+    figure('Position',[130 240 250 440]);  hold on;
+    [sSame,usSame] = ParseInfoStability(fulldataset,'time','ti',teal,'early');
+    [sDiff,usDiff] = ParseInfoStability(fulldataset,'time','si',purple,'early');
+    MakeErrBar(sDiff,usDiff,purple);
+    MakeErrBar(sSame,usSame,teal);
+    ylabel('Median Norm. Mutual Info. [z-scored bits]');
+    xlabel('Temporal Stability','Color',teal);
+    if saveBool
+        print(TimeTI,'-dpdf');
+    end
+    
+    %Run ANOVA.
+    disp('Running Two-Way ANOVA of MI based on temporal stability for early sequence.');
+    ANOVAit(sSame,usSame,sDiff,usDiff);
+    
+    %Check calcium event frequency.
+    figure('Position',[400 240 250 440]); hold on;
+    [S,US] = ParseInfoStability(fulldataset,'time','fr',[.7 .7 .7],'early');
+    MakeErrBar(S,US,[.7 .7 .7]);
+    title(['P = ',num2str(ranksum(S,US))]);
+    ylabel('Norm. Ca Event Freq.');
+    xlabel('Temporal Stability','Color',teal);
+    if saveBool
+        print(TimeFR,'-dpdf');
+    end
+    
+%% Main analysis - late.
+    disp(' ');
+    figure('Position',[670 240 250 440]);  hold on;
+    [sSame,usSame] = ParseInfoStability(fulldataset,'time','ti',teal,'late');
+    [sDiff,usDiff] = ParseInfoStability(fulldataset,'time','si',purple,'late');
+    MakeErrBar(sDiff,usDiff,purple);
+    MakeErrBar(sSame,usSame,teal);
+    ylabel('Median Norm. Mutual Info. [z-scored bits]');
+    xlabel('Temporal Stability','Color',teal);
+    if saveBool
+        print(TimeTI,'-dpdf');
+    end
+    
+    %Run ANOVA.
+    disp('Running Two-Way ANOVA of MI based on temporal stability for late sequence.');
+    ANOVAit(sSame,usSame,sDiff,usDiff);
+    
+    %Check calcium event frequency.
+    figure('Position',[940 240 250 440]); hold on;
+    [S,US] = ParseInfoStability(fulldataset,'time','fr',[.7 .7 .7],'late');
+    MakeErrBar(S,US,[.7 .7 .7]);
+    title(['P = ',num2str(ranksum(S,US))]);
+    ylabel('Norm. Ca Event Freq.');
+    xlabel('Temporal Stability','Color',teal);
+    if saveBool
+        print(TimeFR,'-dpdf');
+    end
+    
+
+%%
+function [sM,usM] = ParseInfoStability(fulldataset,stabilityType,infoType,c,trdmllTime)
+    animals = unique({fulldataset.Animal});
+    nAnimals = length(animals);
+
+    [stats,~,stable,unstable] = StabilityStatsEarlyLate(fulldataset,stabilityType,infoType,trdmllTime);
+    for a=1:nAnimals
+        ssns = find(strcmp(animals{a},{fulldataset.Animal}));
+        
+        [i,j] = deal(1);
+        for s=1:length(ssns)-1
+            sExtent = length(stable{a}{s}); 
+            usExtent = length(unstable{a}{s});
+            
+            if sExtent > 2, sM{a}(s) = median(stats.stable{a}(i:i+sExtent-1));
+            else, sM{a}(s) = nan; end
+                
+            if usExtent > 2, usM{a}(s) = median(stats.unstable{a}(j:j+usExtent-1)); 
+            else, usM{a}(s) = nan; end
+            
+            i = i+sExtent;
+            j = j+usExtent;
+        end
+    end
+    
+    sM = cell2mat(sM)';
+    usM = cell2mat(usM)';
+    
+    plot([1,2],[sM,usM],'color',[c .5]);
+    set(gca,'xticklabel',{'Stable','Unstable'});
+    set(gca,'xtick',[1:2],'tickdir','out');
+    %title(['P = ',num2str(p)]);
+    xlim([0.5,2.5]);
+end
+
+%%
+function [sM,usM,sSEM,usSEM] = MakeErrBar(stable,unstable,c)
+    nSessions = length(stable); 
+    sM = nanmean(stable);          sSEM  = nanstd(stable)./sqrt(nSessions);
+    usM = nanmean(unstable);       usSEM = nanstd(unstable)./sqrt(nSessions); 
+    
+    errorbar([1,2],[sM,usM],[sSEM,usSEM],'color',c,'linewidth',5);
+end
+
+%%
+function ANOVAit(sSame,usSame,sDiff,usDiff)
+    nSessions = length(sSame);
+    
+    dimension = [ones(nSessions*2,1); zeros(nSessions*2,1)];
+    stability = repmat([ones(nSessions,1); zeros(nSessions,1)],[2,1]);
+    grps = {dimension,stability};
+    X = [sSame; usSame; sDiff; usDiff];
+    [~,tbl,stats] = anovan(X,grps,'model','full','varnames',...
+        {'Dimension','Stability'},'display','off');
+    comps = multcompare(stats,'dimension',[1,2],'display','off','ctype','hsd');
+    
+    disp(['Main effect of dimension F = ',num2str(tbl{2,6}),', P = ',num2str(tbl{2,7})]);
+    disp(['Main effect of stability F = ',num2str(tbl{3,6}),', P = ',num2str(tbl{3,7})]);
+    disp(['Interaction F = ',num2str(tbl{4,6}),', P = ',num2str(tbl{4,7})]);
+    disp(['Difference between stable and unstable, same dimension P = ',num2str(comps(5,6))]);
+    disp(['Difference between stable and unstable, opposite dimension P = ',num2str(comps(2,6))]);
+    
+end
---
+++ StabilityOverDays.m
@@ -0,0 +1,85 @@
+function StabilityOverDays(mds)
+%
+%
+%
+
+%%
+    animals = unique({mds.Animal});
+    nAnimals = length(animals); 
+    B = 100;
+    
+    [corrs,shuffleCorrs] = deal(cell(nAnimals,1));
+    for a=1:nAnimals
+        %Get all the sessions for this animal.
+        ssns = find(strcmp(animals{a},{mds.Animal})); 
+        nSessions = length(ssns);
+        
+        [corrs{a},shuffleCorrs{a}] = deal(cell(nSessions-1,1));
+        TCs = getTimeCells(mds(ssns(1)));
+        
+        for s=2:nSessions
+            disp(['Analyzing ',mds(ssns(s)).Animal,' on ',mds(ssns(s)).Date,'.']);
+            cd(mds(ssns(s)).Location);
+                       
+            TCs = EliminateUncertainMatches([mds(ssns(1)) mds(ssns(s))],TCs);
+            
+            temp = CorrTrdmllTrace(mds(ssns(1)),mds(ssns(s)),TCs);
+            corrs{a}{s-1} = temp(:,1);
+            
+            %Preallocate.
+            shuffleCorrs{a}{s-1} = nan(size(temp,1),B);
+            
+            %Progress bar. 
+            resolution = 2;
+            updateInc = round(B/(100/resolution));
+            p = ProgressBar(100/resolution);
+            for i=1:B
+                temp = CorrTrdmllTrace(mds(ssns(1)),mds(ssns(s)),TCs,'shuffle',true);
+                shuffleCorrs{a}{s-1}(:,i) = temp(:,1);
+                
+                if round(i/updateInc) == (i/updateInc), p.progress; end
+            end
+        end
+        
+        %Mean of all surrogate correlations. Rows = sessions,
+        meanShuffleCorrs{a} = cell2mat(cellfun(@(x) nanmean(x),shuffleCorrs{a},'unif',0));
+        meanCorrs{a} = cellfun(@(x) x(1), cellfun(@(x) nanmean(x),corrs{a},'unif',0));
+    end
+    
+%% 
+    %Reorganize the mean shuffled correlations. 
+    daysOut = max(cellfun(@(x) x(1), cellfun(@size, meanShuffleCorrs,'unif',0)));
+    [r,m,color,p] = deal(cell(daysOut,1));
+    c = parula(nAnimals); 
+    for a=1:nAnimals
+        for d=1:size(meanShuffleCorrs{a},1)
+            r{d} = [r{d}, meanShuffleCorrs{a}(d,:)];
+            m{d} = [m{d}, meanCorrs{a}(d)];
+            color{d} = [color{d}; c(a,:)];
+            %p{d} = [p{d}, sum(meanCorrs{a}(d)<meanShuffleCorrs{a}(d,:))/size(meanShuffleCorrs{a},2)];
+        end
+    end
+            
+    %Get mean of all correlation means.
+    M = cellfun(@mean,m);
+    
+    %Sort. 
+    sortedR = cellfun(@sort,r,'unif',0); 
+    Mr = cellfun(@mean,r);
+    %Lerr = Mr - cellfun(@(x) x(round(.05*length(x))),sortedR);
+    %Uerr = cellfun(@(x) x(round(.95*length(x))),sortedR) - Mr;
+    
+    figure('Position',[610 270 400 350]); hold on;
+    b = bar([1:daysOut],M,'facecolor','none','linewidth',2);
+    w = b.BarWidth;
+    for d=1:daysOut
+        line([d-w/2 d+w/2],[Mr(d) Mr(d)],'color','r','linewidth',2);
+        errorbar(d,M(d),std(m{d})/sqrt(length(m{d})),'linewidth',2,'color','k');
+        scat = scatter(d*ones(length(m{d}),1),m{d}',[],color{d},'d','filled');
+        alpha(scat,.5);
+    end
+    set(gca,'xtick',[1:daysOut],'tickdir','out');
+    xlabel('Days from Reference'); 
+    ylabel('Mean Corr. Coeff.');
+    set(gca,'linewidth',2)
+end
