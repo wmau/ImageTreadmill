9a80d1583743a396bb37f7ce7b0a444fc4730d28
wmau <wmau@bu.edu>
2017-02-21 16:11:21 -0500

    Place field analysis now omits 2s before and after treadmill running
    This prevents bleeding over effects from time cell activation.

--- Helpers/batchPFA.m
+++ Helpers/batchPFA.m
@@ -14,6 +14,8 @@ function batchPFA(MDs,excluderuns)
 
 %% For each session, get indices of treadmill running and run Placefields. 
     nSessions = length(MDs); 
+    nSeconds = 2;
+    extraExclude = 20*nSeconds;
     
     for s=1:nSessions
         disp(['Analyzing ',MDs(s).Animal,' on ',MDs(s).Date,', session ',...
@@ -25,15 +27,23 @@ function batchPFA(MDs,excluderuns)
         if excluderuns            
             load(fullfile(pwd,'TimeCells.mat'),'TodayTreadmillLog'); 
             inds = TodayTreadmillLog.inds;
+            
+            load('Pos_align.mat','PSAbool');
+            nFrames = size(PSAbool,2);
                        
             for l=1:size(inds,1)
-                excludeframes = [excludeframes, inds(l,1):inds(l,2)];
+                window = (inds(l,1)-extraExclude):(inds(l,2)+extraExclude);
+                window = window(window>1);
+                window = window(window<nFrames);
+                
+                excludeframes = [excludeframes, window];
             end
         end
         
         %Make place fields. 
         Placefields(MDs(s),'exclude_frames',excludeframes,...
-            'Tenaspis_data','FinalOutput.mat');
+            'Tenaspis_data','FinalOutput.mat','cmperbin',1);
+        PlacefieldStats(MDs(s));
     end
     
 end
