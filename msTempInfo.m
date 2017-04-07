45d33389db2a46230cc1425b7116fa117f2298d3
wmau <wmau@bu.edu>
2017-04-07 18:04:07 -0400

    Temporal information across multiple days

---
+++ msTempInfo.m
@@ -0,0 +1,27 @@
+function [TI,cellID] = msTempInfo(mds,neurons)
+%
+%
+%
+
+%%
+    nSessions = length(mds);
+    nNeurons = length(neurons);
+    
+    %Map cells. 
+    mapMD = getMapMD(mds);
+    map = msMatchCells(mapMD,mds,neurons,true);
+    cellID = map(:,1);
+    
+    TI = nan(nNeurons,nSessions);
+    for s=1:nSessions
+        cd(mds(s).Location); 
+        load('TemporalInfo.mat','MI');
+        
+        for n=1:nNeurons
+            if map(n,s)>0 
+                TI(n,s) = MI(map(n,s)); 
+            end
+        end
+    end
+    
+end
