fb3bc869b559504e511de1cee4711fabe9a2f8e4
wmau <wmau@bu.edu>
2017-04-19 17:21:47 -0400

    Marc's analyses

--- PVTrialCorr.m
+++ PVTrialCorr.m
@@ -1,4 +1,4 @@
-function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
+function [R,p,lapNum,sessionNum] = PVTrialCorr(mds,varargin)
 %[R,p,dayMeans] = PVTrialCorr(mds,varargin)
 %
 %   Performs pairwise correlations on population vectors for each treadmill
@@ -8,7 +8,7 @@ function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
 %% Parse inputs.
     p = inputParser;
     p.addRequired('mds',@(x) isstruct(x)); 
-    p.addParameter('useIntervals',false,@(x) islogical(x));
+    p.addParameter('useIntervals',false,@(x) islogical(x)); 
     p.addParameter('timeCellsOnly',true,@(x) islogical(x)); 
     
     p.parse(mds,varargin{:});
@@ -18,18 +18,18 @@ function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
 %% Gather data across days.
     %Get all the time cells that existed among all recording sessions.
     nSessions = length(mds); 
-    TCs = cell(nSessions,1);
+    cellsOfInterest = cell(nSessions,1);
     nTrials = zeros(nSessions,1);
     for s=1:nSessions
         cd(mds(s).Location);
         
         %Get time cells. 
         if timeCellsOnly
-            TCs{s} = getTimeCells(mds(s));
+            cellsOfInterest{s} = getTimeCells(mds(s));
              %Or just all the neurons. 
         else
             load('FinalOutput.mat','NumNeurons');
-            TCs{s} = 1:NumNeurons;
+            cellsOfInterest{s} = 1:NumNeurons;
         end
         
         %Get number of laps.
@@ -40,33 +40,33 @@ function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
     totalTrials = sum(nTrials);
     
     %Map the time cells.
-    map = msMatchMultiSessionCells(mds,TCs); 
-    nTCs = size(map,1);
+    map = msMatchMultiSessionCells(mds,cellsOfInterest); 
+    nNeurons = size(map,1);
     
 %% Get significance intervals. 
-    sigInterval = cell(nTCs,1); 
+    sigInterval = cell(nNeurons,1); 
     for s=1:nSessions      
         cd(mds(s).Location); 
         
         %Get time cells from this session.
         if timeCellsOnly
-            thisSessionTCs = getTimeCells(mds(s)); 
+            thisSessionCells = getTimeCells(mds(s)); 
         %Or just all the neurons. 
         else
             load('FinalOutput.mat','NumNeurons');
-            thisSessionTCs = 1:NumNeurons;
+            thisSessionCells = 1:NumNeurons;
         end
         
         %Get position in matrix.
         neurons = map(:,s); 
-        isTC = ismember(neurons,thisSessionTCs);
+        countMe = ismember(neurons,thisSessionCells);
         stillEmpty = cellfun('isempty',sigInterval);
-        isTC = isTC & stillEmpty;
+        countMe = countMe & stillEmpty;
         
         cd(mds(s).Location);
         load('TreadmillTraces.mat','DFDTTrdmll');
         if useIntervals
-            for n=find(isTC)'
+            for n=find(countMe)'
                 flat = mean(DFDTTrdmll(:,:,neurons(n)));
                 sd = std(flat); 
 
@@ -80,7 +80,8 @@ function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
     
 %% Grab PVs.
     %Make big ass matrix.
-    PVs = nan(nTCs,totalTrials);
+    PVs = nan(nNeurons,totalTrials);
+    [lapNum,sessionNum] = deal(nan(totalTrials,1));
     t = 1;
     for s=1:nSessions
         %Load traces. 
@@ -92,18 +93,21 @@ function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
         neurons = map(:,s);
         mapped = find(neurons>0);
         neurons = neurons(mapped);
-        nNeurons = length(mapped);
+        nMapped = length(mapped);
         
         %Capture traces. 
         nLaps = size(DFDTTrdmll,1); 
         for l=1:nLaps
-            for n=1:nNeurons                   
-                cellTraces = DFDTTrdmll(l,sigInterval{n},neurons(n)); 
-                meanActivity = mean(cellTraces);
+            for n=1:nMapped                   
+                cellTrace = DFDTTrdmll(l,sigInterval{n},neurons(n)); 
+                meanActivity = mean(cellTrace);
                 PVs(mapped(n),t) = meanActivity;
             
             end
-            t = t+1;
+            lapNum(t) = l;
+            sessionNum(t) = s;
+            
+            t = t+1;  
         end
     end
     
@@ -111,7 +115,7 @@ function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
     m = nanmean(PVs,2);
     sd = nanstd(PVs,[],2);
     
-    for n=1:nTCs
+    for n=1:nNeurons
         PVs(n,:) = bsxfun(@minus,PVs(n,:),m(n));
         PVs(n,:) = bsxfun(@rdivide,PVs(n,:),sd(n));
     end
@@ -124,20 +128,14 @@ function [R,p,dayMeans] = PVTrialCorr(mds,varargin)
     imap = imagesc(R); colormap jet; 
     set(imap,'alphadata',~isnan(R));
     hold on;
-    lines = cumsum(nTrials);
-    lines = [1; lines];
+    newDayStartInd = cumsum(nTrials);
+    newDayStartInd = [1; newDayStartInd];
 %     line([0 0],[0,totalTrials],'color','k','linewidth',4);
 %     line([0,totalTrials],[1 1],'color','k','linewidth',4);
     for s=1:nSessions+1
-        line([lines(s) lines(s)],[0,totalTrials],'color','k','linewidth',4);
-        line([0,totalTrials],[lines(s) lines(s)],'color','k','linewidth',4);
+        line([newDayStartInd(s) newDayStartInd(s)],[0,totalTrials],'color','k','linewidth',4);
+        line([0,totalTrials],[newDayStartInd(s) newDayStartInd(s)],'color','k','linewidth',4);
     end
     axis equal; axis tight;
     
-    dayMeans = zeros(nSessions,1);
-    for s=1:nSessions
-        thisSessionRs = R(lines(s):lines(s+1),1:lines(2)); 
-        
-        dayMeans(s) = nanmean(nanmean(thisSessionRs));
-    end
 end
