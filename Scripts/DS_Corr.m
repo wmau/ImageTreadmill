3fbdcfa9edede573834b5cb028c18cf7bd4d4c8f
wmau <wmau@bu.edu>
2017-07-12 13:01:52 -0400

    Now uses transients to compute DS

--- Scripts/DS_Corr.m
+++ Scripts/DS_Corr.m
@@ -3,25 +3,38 @@ loadMD;
 
 fulldataset = MD(292:309); 
 nSessions = length(fulldataset); 
+neuralActivity = 'transient';
 
-[tDS,pDS] = deal(cell(nSessions,1));
+[tDS,pDS,TI,SI,sigT,sigP] = deal(cell(nSessions,1));
 for s=1:nSessions
     disp(['Analyzing ',fulldataset(s).Animal,' ',fulldataset(s).Date]);
     neurons = AcquireTimePlaceCells(fulldataset(s),'dual');
     
-    tDS{s} = DecodeTimeWithSpatialLookup(fulldataset(s),'plotit',false,'neurons',neurons);    
-    pDS{s} = DecodePlaceWithTemporalLookup(fulldataset(s),'plotit',false,'neurons',neurons);
+    [tDS{s},sigT{s}] = DecodeTimeWithSpatialLookup(fulldataset(s),...
+        'plotit',false,'neurons',neurons,'neuralActivity',neuralActivity);    
+    [pDS{s},sigP{s}]= DecodePlaceWithTemporalLookup(fulldataset(s),...
+        'plotit',false,'neurons',neurons,'neuralActivity',neuralActivity);
+    
+    cd(fulldataset(s).Location);
+    load('TemporalInfo.mat','MI');
+    TI{s} = MI;
+    load('SpatialInfo.mat','MI');
+    SI{s} = MI; 
 end
 
 T = cell2mat(tDS);
 P = cell2mat(pDS); 
+TI = cell2mat(TI);
+SI = cell2mat(SI); 
 
 [r,p] = corr(P,T,'rows','complete');
 figure;
-scatter(P,T,40,[0 .5 .5],'filld'); 
+s = scatter(P,T,50,'k');
+alpha(s,.5);
+lsline;
 title(['R=',num2str(r),', p=',num2str(p)],'fontsize',15);
 xlabel('Spatial DS','fontsize',15);
 ylabel('Temporal DS','fontsize',15); 
 set(gca,'tickdir','out','fontsize',12,'linewidth',4);
-xlim([0 1]);
-ylim([0 1]);
+% xlim([0 1]);
+% ylim([0 1]);
