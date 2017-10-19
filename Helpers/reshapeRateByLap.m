3add160d767cc7730390bea6526039435b277843
wmau <wmau@bu.edu>
2017-10-19 18:16:32 -0400

    Potentially impactful indexing change. Also now accommodates trial decoding.
    Changed how laps are randomly sampled. Rather than sampling from a
    vector of laps where the run was completed, first trim away the
    uncompleted laps then sample from those.

--- Helpers/reshapeRateByLap.m
+++ Helpers/reshapeRateByLap.m
@@ -10,30 +10,49 @@ function X = reshapeRateByLap(md,varargin)
     load('TimeCells.mat','ratebylap','TodayTreadmillLog');
     complete = logical(TodayTreadmillLog.complete);         %Completed runs.
     nRuns = round(sum(complete)*.5);                        %Number of runs to use in model.
-    randRuns = randsample(sum(complete),nRuns)';            %Subset of runs. 
+    randRuns = randsample(find(complete),nRuns)';            %Subset of runs. 
     
     p = inputParser; 
     p.addRequired('md',@(x) isstruct(x)); 
     p.addParameter('neurons',getTimeCells(md),@(x) isnumeric(x));
-    p.addParameter('runs',randRuns,@(x) isnumeric(x)); 
+    p.addParameter('runs',randRuns,@(x) isnumeric(x));
+    p.addParameter('shuffle',false,@(x) islogical(x)); 
+    p.addParameter('collapseTrials',false,@(x) islogical(x)); 
 
     p.parse(md,varargin{:});
     neurons = p.Results.neurons;
     runs = p.Results.runs; 
+    shuffle = p.Results.shuffle;
+    collapseTrials = p.Results.collapseTrials; 
     
     %Flip vector into column vector. 
     if size(neurons,1) > size(neurons,2)
         neurons = neurons';
     end
 %% Reshape ratebylap. 
+    ratebylap = ratebylap(complete,:,:);
     ratebylap = ratebylap(runs,:,neurons);                  %Only take specified runs.
     ratebylap(ratebylap > 0) = 1; 
+    
     [nRuns,nBins,nNeurons] = size(ratebylap);              	%Size variables. 
     
+    if shuffle 
+        ratebylap = ratebylap(:,:,randperm(nNeurons));
+    end
+    
     %Preallocate then reshape. 
-    X = nan(nRuns*nBins,nNeurons);
-    for n=1:nNeurons
-        temp = ratebylap(:,:,n)';           %Get that neuron's raster. 
-        X(:,n) = temp(:);                   %Flatten. 
+    if collapseTrials
+        X = nan(nRuns,nNeurons);
+        
+        for n=1:nNeurons
+            temp = ratebylap(:,:,n);
+            X(:,n) = sum(temp,2);
+        end
+    else
+        X = nan(nRuns*nBins,nNeurons);
+        for n=1:nNeurons
+            temp = ratebylap(:,:,n)';           %Get that neuron's raster. 
+            X(:,n) = temp(:);                   %Flatten. 
+        end
     end
 end
--- TimeDecoder.m
+++ TimeDecoder.m
@@ -1,4 +1,4 @@
-function [Mdl,decodedTime,postProbs] = TimeDecoder(md,varargin)
+function [Mdl,X,testX,testLaps] = TimeDecoder(md,varargin)
 %
 %
 %
@@ -14,23 +14,24 @@ function [Mdl,decodedTime,postProbs] = TimeDecoder(md,varargin)
     p.addParameter('nTrainingRuns',round(sum(complete)*.5),...
         @(x) isnumeric(x)); 
     p.addParameter('neurons',getTimeCells(md),@(x) isnumeric(x)); 
-    p.addParameter('plotit',true,@(x) islogical(x)); 
+    p.addParameter('shuffle',false,@(x) islogical(x)); 
     
     p.parse(md,varargin{:});
     nTrainingRuns = p.Results.nTrainingRuns;
     neurons = p.Results.neurons; 
-    plotit = p.Results.plotit;
+    shuffle = p.Results.shuffle;
 
 %% Fit the model.
     %Take a random subset of laps. This is our training data. 
-    trainingLaps = randsample(find(complete),nTrainingRuns)';
+    trainingLaps = randsample(sum(complete),nTrainingRuns)';
     
     %The remaining laps are our test laps. 
-    testLaps = setdiff(find(complete),trainingLaps);
+    testLaps = setdiff(sum(complete),trainingLaps);
     
     %Get predictor matrix and the test matrix. 
     X = reshapeRateByLap(md,'runs',trainingLaps,'neurons',neurons); 
-    testX = reshapeRateByLap(md,'runs',testLaps,'neurons',neurons); 
+    testX = reshapeRateByLap(md,'runs',testLaps,'neurons',neurons,...
+        'shuffle',shuffle); 
     
     %Get response vector. 
     t = linspace(0,10,nBins)'; 
@@ -39,6 +40,4 @@ function [Mdl,decodedTime,postProbs] = TimeDecoder(md,varargin)
     %Fit the model. 
     Mdl = fitcnb(X,Y,'distributionnames','mn');
     
-%% Decode. 
-   [decodedTime,postProbs] = PredictTime(Mdl,testX,'plotit',plotit); 
 end
