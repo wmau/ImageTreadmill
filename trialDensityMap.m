506daf26182066b5493c65a68b416156f34eba4e
William Mau <wmau@bu.edu>
2018-01-16 09:31:58 -0500

    Trial density maps

--- trialDensityMap.m
+++ trialDensityMap.m
@@ -1,22 +1,30 @@
-function trialDensityMap(md,varargin)
+function [TCtrialDensity,neurons,order] = trialDensityMap(md,varargin)
 %
 %
 %
 
 %%
-    p = inputParser;
-    p.addRequired('md',@(x) isstruct(x)); 
-    p.addParameter('nTrialBlocks',10,@(x) isscalar(x)); 
-    
-    p.parse(md,varargin{:});
-    nTrialBlocks = p.Results.nTrialBlocks; 
-    
-%%
+    neurons = getTimeCells(md);
     load(fullfile(md.Location,'TimeCells.mat'),'TodayTreadmillLog','ratebylap',...
         'curves'); 
     complete = logical(TodayTreadmillLog.complete);
+    nTrialBlocks = sum(complete); 
+    
+    p = inputParser;
+    p.addRequired('md',@(x) isstruct(x)); 
+    p.addParameter('nTrialBlocks',nTrialBlocks,@(x) isscalar(x)); 
+    p.addParameter('neurons',neurons,@(x) isnumeric(x));
+    p.addParameter('order',[]); 
+    
+    p.parse(md,varargin{:});
+    nTrialBlocks = p.Results.nTrialBlocks; 
+    neurons = p.Results.neurons;
+    order = p.Results.order;
+    
+%%
     ratebylap = ratebylap(complete,:,:);
     [nRuns,~,nNeurons] = size(ratebylap); 
+    ratebylap = ratebylap > 0;
     sig = curves.sig; 
     
     trialBlockLims = linspace(0,nRuns,nTrialBlocks+1); 
@@ -29,19 +37,26 @@ function trialDensityMap(md,varargin)
         for b=1:nTrialBlocks
             block = trialBlockLims(b)+1:trialBlockLims(b+1);
             
-            trialDensity(neuron,b) = mean(sum(ratebylap(block,goodbins,neuron),2));
+            trialDensity(neuron,b) = mean(any(ratebylap(block,goodbins,neuron),2));
         end
     end
     
-    TimeCells = getTimeCells(md);
-    TCtrialDensity = trialDensity(TimeCells,:);
+    TCtrialDensity = trialDensity(neurons,:);
     TCtrialDensity = TCtrialDensity./max(TCtrialDensity,[],2);
+    neverActive = ~any(TCtrialDensity,2); 
     [~,ind] = max(TCtrialDensity,[],2);
-    [~,order] = sort(ind);
+    TCtrialDensity(neverActive,:) = nan;
     
-    figure('position',[825     10   260   990]);
-    imagesc(TCtrialDensity(order,:));
-    axis equal; axis tight;
+    if isempty(order)
+        skewness = getAllSkewnesses(md);
+        [~,order] = sort(skewness(neurons));
+    end
+    
+    %figure('position',[825     10   260   990]);
+    h = imagesc(TCtrialDensity(order,:));
+    set(h,'AlphaData',~isnan(TCtrialDensity(order,:)));
+    %axis equal; 
+    axis tight;
     xlabel('Trial blocks');
     ylabel('Cell #');
     set(gca,'fontsize',14,'xtick',[]);
